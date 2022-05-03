b298104f47a209d7466d2e5f7b3aaf897d1504a2
HangSub999 <gkdtjq999@gmail.com>
2022-05-04 00:45:22 +0900

    feat: 사용자가 입력한 회원 정보를 저장하는 클래스

---
+++ Member.java
@@ -0,0 +1,34 @@
+public class Member {
+
+    private String memberId;
+    private String name;
+    private String phoneNumber;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString(){
+        return "Member [memberId="+memberId+", name="+name+", phoneNumber="+phoneNumber+"]";
+    }
+}
